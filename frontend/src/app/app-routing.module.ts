7a953d00c99fad8f44152885c943dc8ae26b394b
Senia <82032813+SSSenia@users.noreply.github.com>
2023-01-27 23:38:35 +0200

    Add routing to the application

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurrentPageComponent } from './content/current-page/current-page.component';
+import { HelpPageComponent } from './content/help-page/help-page.component';
+import { StatisticPageComponent } from './content/statistic-page/statistic-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/current', pathMatch: 'full' },
+  { path: 'current', component: CurrentPageComponent },
+  { path: 'statistic', component: StatisticPageComponent },
+  { path: 'help', component: HelpPageComponent },
+  { path: '**', redirectTo: 'current' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
