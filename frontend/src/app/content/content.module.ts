b2232d2a8ab06c53ef22f41ae973a8f3564b158c
AnnaShS <shutkoanua6168@gmail.com>
2023-01-28 23:42:18 +0200

    Add localization and language switching on all pages

--- frontend/src/app/content/content.module.ts
+++ frontend/src/app/content/content.module.ts
@@ -4,6 +4,7 @@ import { CurrentPageComponent } from './current-page/current-page.component';
 import { HelpPageComponent } from './help-page/help-page.component';
 import { StatisticPageComponent } from './statistic-page/statistic-page.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
@@ -15,7 +16,8 @@ import { MatSliderModule } from '@angular/material/slider';
   ],
   imports: [
     CommonModule,
-    MatSliderModule
+    MatSliderModule,
+    TranslateModule
   ]
 })
 export class ContentModule { }
