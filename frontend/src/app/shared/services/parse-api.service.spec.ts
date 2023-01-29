75a052614a91214ed2079e4c6430fc16767cdb7c
Senia <82032813+SSSenia@users.noreply.github.com>
2023-01-29 15:57:24 +0200

    Change getting data from api when requesting last-values

--- frontend/src/app/shared/services/parse-api.service.spec.ts
+++ frontend/src/app/shared/services/parse-api.service.spec.ts
@@ -1,5 +1,5 @@
 import { of } from "rxjs";
-import { ICurrentValues, ILastValues } from "../interfaces";
+import { IApiValues, IChartValues, ICurrentValues } from "../interfaces";
 import { ParseApiService } from "./parse-api.service";
 
 describe('ParseApiService', () => {
@@ -20,18 +20,45 @@ describe('ParseApiService', () => {
   });
 
   it('should modify object correctly', () => {
-    const testObj: ILastValues = {
-      temperature: [{ value: [new Date(123), 12] }],
-      pressure: [{ value: [new Date(123), 23] }],
-      humidity: [{ value: [new Date(123), 34] }],
+    const inputObj: IApiValues = {
+      realData: [
+        {
+          temperature: 12,
+          pressure: 23,
+          humidity: 34,
+          date: new Date(123)
+        }
+      ],
+      prognosisData: [
+        {
+          temperature: 45,
+          pressure: 56,
+          humidity: 67,
+          date: new Date(1234)
+        }
+      ]
     };
 
-    const spy = jasmine.createSpyObj('HttpClient', { get: of(testObj) });
+    const outputObj: IChartValues = {
+      realData: {
+        temperature: [{ value: [new Date(123), 12] }],
+        pressure: [{ value: [new Date(123), 23] }],
+        humidity: [{ value: [new Date(123), 34] }],
+      },
+      prognosisData: {
+        temperature: [{ value: [new Date(1234), 45] }],
+        pressure: [{ value: [new Date(1234), 56] }],
+        humidity: [{ value: [new Date(1234), 67] }],
+      }
+    };
+
+    const spy = jasmine.createSpyObj('HttpClient', { get: of(inputObj) });
     const parseApiService: ParseApiService = new ParseApiService(spy);
 
     parseApiService.getLastValues(1).subscribe((value) => {
-      expect(value).toEqual(testObj);
+      expect(value).toEqual(outputObj);
     }).unsubscribe();
   });
+
 
 });
--- frontend/src/app/shared/services/parse-api.service.ts
+++ frontend/src/app/shared/services/parse-api.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { IChartValue, ICurrentValues, ILastValues } from '../interfaces';
+import { map, Observable, of } from 'rxjs';
+import { IArrayData, IChartValue, IChartValues, ICurrentValues, IApiValues, IApiValue } from '../interfaces';
 
-const IS_MOCK: boolean = false;
+const IS_MOCK: boolean = true;
 const API_PATH: string = '/api';
 
 @Injectable({
@@ -14,17 +14,24 @@ export class ParseApiService {
   constructor(
     private http: HttpClient
   ) { }
-  
+
   mockLastValues: IChartValue[] = [];
 
-  getMockLastValues(amount: number): Observable<ILastValues> {
+  getMockLastValues(amount: number): Observable<IChartValues> {
     const value = 24 + (Math.random() - 0.5) * 5;
     this.mockLastValues.push({ value: [new Date, value] });
     const mockData = this.mockLastValues.slice(-amount);
     return of({
-      temperature: mockData,
-      pressure: mockData,
-      humidity: mockData
+      realData: {
+        temperature: mockData,
+        pressure: mockData,
+        humidity: mockData
+      },
+      prognosisData: {
+        temperature: [],
+        pressure: [],
+        humidity: []
+      }
     });
   }
 
@@ -42,10 +49,33 @@ export class ParseApiService {
     return this.http.get<ICurrentValues>(API_PATH + '/current');
   }
 
-  getLastValues(amount: number): Observable<ILastValues> {
+  getLastValues(amount: number): Observable<IChartValues> {
     if (IS_MOCK) return this.getMockLastValues(amount);
-    return this.http.get<ILastValues>(API_PATH + '/last-values', {
+    const reduceFunc = (prev: IArrayData, current: IApiValue) => {
+      prev.temperature.push({ value: [current.date, current.temperature] });
+      prev.pressure.push({ value: [current.date, current.pressure] });
+      prev.humidity.push({ value: [current.date, current.humidity] });
+      return prev;
+    };
+    return this.http.get<IApiValues>(API_PATH + '/last-values', {
       headers: { amount: String(amount) }
-    });
+    }).pipe(
+      map((statisticValues: IApiValues) => ({
+        realData: {
+          ...statisticValues.realData.reduce(reduceFunc, {
+            temperature: [],
+            pressure: [],
+            humidity: []
+          })
+        },
+        prognosisData: {
+          ...statisticValues.prognosisData.reduce(reduceFunc, {
+            temperature: [],
+            pressure: [],
+            humidity: []
+          })
+        }
+      }))
+    );
   }
 }
