917fa2b1db5a0ef9843b5e0287bf2f0d53c1bbc6
Pavlo <meg4mag@gmail.com>
2023-01-29 12:59:19 +0200

    Update models.py

--- backend/rrms_web_app/models.py
+++ backend/rrms_web_app/models.py
@@ -1,3 +1,10 @@
-from django.db import models
 
+from djongo import models
 # Create your models here.
+
+class MainData(models.Model):
+    _id = models.ObjectIdField()
+    temperature = models.FloatField()
+    pressure = models.FloatField()
+    humidity = models.FloatField()
+    timeadata = models.DateTimeField()
