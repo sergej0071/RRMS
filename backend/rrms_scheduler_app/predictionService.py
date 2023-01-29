087be200900801e6b35cbc4c086ebef7b4969d44
Serhii Denysenko <serhii.denysenko.consultant@msccruises.com>
2023-01-29 22:31:45 +0200

    RRMS-69: addede a polinomial functionality.

--- backend/rrms_scheduler_app/predictionService.py
+++ backend/rrms_scheduler_app/predictionService.py
@@ -1,10 +1,30 @@
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import datetime
 import numpy as np
 import pandas as pd
+import logging
 
-class PredictionService():        
+class PredictionService():
+    def __init__(self):
+        self.logger = logging.getLogger('error_logger')
     def getPredictionValue(self, value, timePeriod, predictAmount = 10, degreeP = 8):  
+
+        if(value == None and len(value) == 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} 
+            method: {nameof(getPredictionValue)} value: {value} None or empty""")
+
+        if(timePeriod == None and timePeriod <= 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} \ 
+            method: {nameof(getPredictionValue)} timePeriod: {timePeriod} None or <= 0""")
+
+        if(predictAmount == None and predictAmount <= 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} \
+            method: {nameof(getPredictionValue)} predictAmount: {predictAmount} None or <= 0""")
+
+        if(degreeP == None and degreeP <= 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} \
+            method: {nameof(getPredictionValue)} degreeP: {degreeP} None or <= 0 """)
 
         conditionalTime = [i for i in range(1, len(value) + 1, timePeriod)]
         arrayTime = np.array(conditionalTime)
@@ -15,18 +35,29 @@ class PredictionService():
         DataFrameValue = pd.DataFrame(arrayValue)
         valueD = DataFrameValue.iloc[:, 0].values
 
+        poly_reg = PolynomialFeatures(degree = degreeP)
+        value_poly = poly_reg.fit_transform(timeValue)
         lin_reg = LinearRegression()
-        lin_reg.fit(timeValue, valueD)
+        lin_reg.fit(value_poly, valueD)
 
         for it in range(len(value), len(value) + predictAmount, timePeriod):
-           value.append(lin_reg.predict[[it]])[0]
+           value.append(lin_reg.predict(poly_reg.fit_transform([[it]]))[0])
 
         return value
 
     def modifyPredictedTime(self, time, timePeriod, predictAmount = 10):
         
-        if(len(time) == 0):
-            return time
+        if(time == None and len(time) == 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} \
+            *method: {nameof(getPredictionValue)} time: {time} None or empty""")
+
+        if(timePeriod == None and timePeriod <= 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} \
+            *method: {nameof(getPredictionValue)} timePeriod: {timePeriod} None or <= 0""")
+
+        if(predictAmount == None and predictAmount <= 0):
+            self.logger.error(f"""exeption behavior in service: {nameof(PredictionService)} \
+            *method: {nameof(getPredictionValue)} predictAmount: {predictAmount} None or <= 0""")
 
         seconds = [i for i in range(1, predictAmount + 1, timePeriod)]
         lastTick = time[-1]
