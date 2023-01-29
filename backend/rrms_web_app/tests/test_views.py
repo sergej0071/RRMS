1e8e51d1b10edf0b3d03b54259df7e61564c0aa2
Pavlo <meg4mag@gmail.com>
2023-01-29 16:51:27 +0200

    Update tests.py

--- backend/rrms_web_app/tests/test_views.py
+++ backend/rrms_web_app/tests/test_views.py
@@ -11,3 +11,11 @@ class CurrentStatusTests(APITestCase):
     def test_wrong_take_current(self):
         response = self.client.post("/current/", {})
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_take_last_values(self):
+        response = self.client.get("/last_values/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_wrong_take_last_values(self):
+        response = self.client.post("/last_values/", {})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
