06a675a9b51f82a87bedd65e75866c808f602161
AnnaShS <shutkoanua6168@gmail.com>
2023-01-29 15:44:28 +0200

    Add the ability to predict data

--- frontend/src/app/shared/interfaces.ts
+++ frontend/src/app/shared/interfaces.ts
@@ -5,20 +5,37 @@ export interface IArticle {
   content: string;
 }
 
+export interface IChartValue {
+  value: [Date, number]
+}
+
+export interface IArrayData {
+  temperature: IChartValue[];
+  pressure: IChartValue[];
+  humidity: IChartValue[];
+}
+
+export interface IChartValues {
+  realData: IArrayData;
+  prognosisData: IArrayData;
+}
+
 export interface ICurrentValues {
   temperature: number;
   pressure: number;
   humidity: number;
 }
 
-export interface IChartValue {
-  value: [Date, number]
+export interface IApiValues {
+  realData: IApiValue[];
+  prognosisData: IApiValue[];
 }
 
-export interface ILastValues {
-  temperature: IChartValue[];
-  pressure: IChartValue[];
-  humidity: IChartValue[];
+export interface IApiValue {
+  temperature: number;
+  pressure: number;
+  humidity: number;
+  date: Date;
 }
 
 export interface IPackageEChartOption {
