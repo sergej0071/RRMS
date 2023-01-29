e1d50c4646f68163eb663001eceb9c0381babf81
Serhii Denysenko <serhii.denysenko.consultant@msccruises.com>
2023-01-29 17:57:58 +0200

    RRMS-64: fix problems with id, added tests

--- backend/rrms_web_app/models.py
+++ backend/rrms_web_app/models.py
@@ -1,9 +1,9 @@
 
-from djongo import models
+from django.db import models
 # Create your models here.
 
 class MainData(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.AutoField(primary_key=True, editable=False)
     temperature = models.FloatField()
     pressure = models.FloatField()
     humidity = models.FloatField()
