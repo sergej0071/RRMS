74c5ff6313382ea1ae2661e687673f66f95d7824
Pavlo <meg4mag@gmail.com>
2023-01-29 13:26:00 +0200

    Update tests.py

--- backend/rrms_web_app/tests.py
+++ backend/rrms_web_app/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
+class CurrentStatusCase(APITestCase):
+    def test_take_current(self):
+        response = self.client.get("/current/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_wrong_take_current(self):
+        response = self.client.post("/current/", {})
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
