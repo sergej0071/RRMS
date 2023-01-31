2032df1c978df48177547157c439e30cec43bf25
Serhii Denysenko <serhii.denysenko.consultant@msccruises.com>
2023-01-31 18:08:10 +0200

    RRMS-92: added a test for views.

--- backend/rrms_web_app/tests/test_views.py
+++ backend/rrms_web_app/tests/test_views.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rrms_web_app.views import LastValues
 
 # Create your tests here.
 class CurrentStatusTests(APITestCase):
@@ -19,3 +20,28 @@ class CurrentStatusTests(APITestCase):
     def test_wrong_take_last_values(self):
         response = self.client.post("/last_values/", {})
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_splitArrayBykey_IsHaveArray_ReturnCorrectArray():
+        #arrange
+        testKey = "testkey"
+        testArray = ["testkey":"test", "testkey1":"test", "testkey2":"test"]
+        expectedArray =  ["testkey": "test"]
+        lastValues = LastValues()
+
+        #act
+        result = lastValues._splitArrayBykey(expectedArray, testKey)
+
+        #assert
+        self.assertEquals(expectedArray, lastValues)
+        testArray, testKey
+
+    def test_splitArrayBykey_IsArrayEmpty_ReturnEmptyArray():
+        #arrange
+        expectedArray = []
+        lastValues = LastValues()
+        
+        #act
+        result = lastValues._splitArrayBykey(expectedArray, "")
+
+        #assert
+        self.assertEquals(expectedValue, result)
