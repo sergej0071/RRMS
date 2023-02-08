6116f3d6c5f806dccc4b356c68aa815693e8fe17
Senia <82032813+SSSenia@users.noreply.github.com>
2023-02-08 23:53:36 +0200

    Update 0001_initial.py

--- backend/rrms_web_app/migrations/0001_initial.py
+++ backend/rrms_web_app/migrations/0001_initial.py
@@ -1,5 +1,4 @@
 from django.db import migrations, models
-import djongo.models.fields
 
 
 class Migration(migrations.Migration):
@@ -13,7 +12,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MainData',
             fields=[
-                ('_id', djongo.models.fields.ObjectIdField(auto_created=True, primary_key=True, serialize=False)),
+                ('_id', models.AutoField(editable=False, primary_key=True, serialize=False)),
                 ('temperature', models.FloatField()),
                 ('pressure', models.FloatField()),
                 ('humidity', models.FloatField()),
