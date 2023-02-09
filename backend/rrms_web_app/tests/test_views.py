5b2c8689d2bdc353306e433e5d8a05a43ef70dff
Pavlo <meg4mag@gmail.com>
2023-02-09 22:09:40 +0200

    fix test

--- backend/rrms_web_app/tests/test_views.py
+++ backend/rrms_web_app/tests/test_views.py
@@ -1,21 +1,21 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.urls import reverse
 
 # Create your tests here.
 class CurrentStatusTests(APITestCase):
     def test_take_current(self):
-        response = self.client.get("/current/")
-        print(response)
+        response = self.client.get('/current/', follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_wrong_take_current(self):
-        response = self.client.post("/current/", {})
+        response = self.client.post("/current/", {}, follow=True)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_take_last_values(self):
-        response = self.client.get("/last_values/")
+        response = self.client.get("/last-values/3", follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_wrong_take_last_values(self):
-        response = self.client.post("/last_values/", {})
+        response = self.client.post("/last-values/3", {} , follow=True)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
--- backend/rrms_web_app/urls.py
+++ backend/rrms_web_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include, re_path
+from django.urls import path
 from . import views
 
-app_name = 'rest_endpoins'
+app_name = 'rrms_web_app'
 
 urlpatterns = [
-    path('current/', views.CurrentStatus.as_view()),
+    path('current/', views.CurrentStatus.as_view(), name="current"),
     path('last-values/<int:amount>', views.LastValues.as_view()),
     path('probability/<int:amount>', views.ProbabilityDiagram.as_view()),
     path('correlation/<int:amount>', views.Correlation.as_view())
